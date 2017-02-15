cb82ed7897d9c295b81c9971799d1b30c5968777
Adam <musician903@gmail.com>
2017-02-14 20:24:23 -0600

    grunt default

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,7 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             build: {
-                src: './piwik',
-                dest: 'building/piwik'
+
             }
         },
         php: {
